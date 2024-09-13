693d08ba0f5607dba692166efffd6eb403f94630
rodrigues <marcos.rodrigues@sh3.com.br>
2024-09-13 14:50:32 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ['http://localhost:5173', 'http://example.com'];
+const allowedOrigins = ['http://localhost:5173', 'https://csteamraid.vercel.app/'];
 
 app.use(cors({
     origin: (origin, callback) => {
